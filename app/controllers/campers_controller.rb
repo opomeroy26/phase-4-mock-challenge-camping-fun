7ea778ba7b136a4d465f39adfe5c7cd0adc4b820
Olivia Pomeroy <opomeroy26@gmail.com>
2022-04-13 22:03:31 -0700

    Completed deliverables

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,25 @@
+class CampersController < ApplicationController
+    
+    def index 
+        campers = Camper.all 
+        render json: campers 
+
+    end
+
+    def show 
+        camper = Camper.find(params[:id])
+        render json: camper, serializer: CamperWithActivitySerializer 
+    end
+
+    def create  
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created 
+
+    end
+
+    private 
+
+    def camper_params 
+        params.permit(:name, :age)
+    end
+end
